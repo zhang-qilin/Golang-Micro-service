7081aa14c797df1a7ab916571750677079129d27
张齐林 <3323816129@qq.com>
2023-02-09 16:14:20 +0800

    服务端添加SSL/TLS认证机制

--- gRPC/hellow-server/main.go
+++ gRPC/hellow-server/main.go
@@ -13,6 +13,7 @@ import (
 	"net"
 	
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // hello server
@@ -26,10 +27,17 @@ func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb. Hello
 }
 
 func main() {
+	// SSL/TSL加密验证
+	creds, _ := credentials.NewServerTLSFromFile("D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.pem", "D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.key")
+	
 	// 开启端口
 	listen, _ := net.Listen("tcp","127.0.0.1:9090")
 	// 创建gRPC服务
-	grpcServer := grpc.NewServer()
+	// grpcServer := grpc.NewServer
+	
+	// 添加SSL/TSL加密验证机制
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
+	
 	// 在gRPC服务端中注册我们自己编写的服务
 	pb.RegisterSayHelloServer(grpcServer,&server{})
 	// 启动服务
