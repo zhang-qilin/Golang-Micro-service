607928b11a4c833e40d5e7b006cd2ea79906071d
张齐林 <3323816129@qq.com>
2023-02-09 17:57:09 +0800

    服务端与客户端添加Token认证（gRPC 本身不提供 Token 认证机制，而是通过插件机制去支持第三方认证）

--- gRPC/hellow-client/main.go
+++ gRPC/hellow-client/main.go
@@ -12,18 +12,44 @@ import (
 	"fmt"
 	
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+
+// ClientTokenAuth 客户端定义的Token验证
+type ClientTokenAuth struct {
+}
+
+// GetRequestMetadata
+func (c ClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":"zhangqili",
+		"appKey":"123456",
+	},nil
+}
+
+func (c ClientTokenAuth) RequireTransportSecurity() bool{
+	return false
+}
+
 func main() {
-	creds, _ := credentials.NewClientTLSFromFile("D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.pem", "*.zhangqilin.com") // *.zhangqilin.com这个域一般不会写死，一般都是通过url去获取的
-	
+	// creds, _ := credentials.NewClientTLSFromFile("D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.pem", "*.zhangqilin.com") // *.zhangqilin.com这个域一般不会写死，一般都是通过url去获取的
+
 	// 连接到server端，此处禁用安全传输，没有加密和验证
 	// grpc.Dial("127.0.0.1:9090",grpc.WithTransportCredentials(grpc.WithInsecure()))  // grpc.WithInsecure()  已被遗弃，可使用以下的insecure.NewCredentials()则代表不使用安全加密
 	// conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	
 	// 使用SSL/TLS加密传输
-	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
+	// conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
+	
+	// token验证
+	var opts []grpc.DialOption
+	// 如果上面的RequireTransportSecurity返回true的话则需要把insecure.NewCredentials()替换成creds
+	// opts = append(opts, grpc.WithTransportCredentials(creds))
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+	
+	conn, err := grpc.Dial("127.0.0.1:9090",opts...)
 	if err != nil {
 		fmt.Sprintf("Did Not Connect: %v\n", err)
 		return
--- gRPC/hellow-server/main.go
+++ gRPC/hellow-server/main.go
@@ -9,11 +9,13 @@ package main
 import (
 	pb "Golang-Micro-service/gRPC/hellow-client/proto"
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 // hello server
@@ -21,30 +23,54 @@ type server struct {
 	pb.UnimplementedSayHelloServer
 }
 
-// SayHello 重写SayHello方法
-func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb. HelloResponse, error) {
+// SayHello 重写SayHello方法(具体业务)
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	// 获取元数据信息
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输Token")
+	}
+	var (
+		appId  string
+		appKey string
+	)
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+		fmt.Println("获取到appID：",appId)
+	}
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+		fmt.Println("获取到appKey：",appKey)
+	
+	}
+	if appId != "zhangqilin" || appKey != "123456" {
+		return nil,errors.New("Token不正确")
+	}
 	return &pb.HelloResponse{ResponseMsg: "hello"+req.RequestName},nil
+	
 }
 
 func main() {
 	// SSL/TSL加密验证
-	creds, _ := credentials.NewServerTLSFromFile("D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.pem", "D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.key")
-	
+	// 自签名证书文件和私钥文件
+	// creds, _ := credentials.NewServerTLSFromFile("D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.pem", "D:\\种子\\github.com\\Golang-Micro-service\\gRPC\\key\\test.key")
+
 	// 开启端口
-	listen, _ := net.Listen("tcp","127.0.0.1:9090")
+	listen, _ := net.Listen("tcp", "127.0.0.1:9090")
 	// 创建gRPC服务
-	// grpcServer := grpc.NewServer
-	
+	// grpcServer := grpc.NewServer()
+
 	// 添加SSL/TSL加密验证机制
-	grpcServer := grpc.NewServer(grpc.Creds(creds))
-	
+	// grpcServer := grpc.NewServer(grpc.Creds(creds))
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+
 	// 在gRPC服务端中注册我们自己编写的服务
-	pb.RegisterSayHelloServer(grpcServer,&server{})
+	pb.RegisterSayHelloServer(grpcServer, &server{})
 	// 启动服务
 	err := grpcServer.Serve(listen)
 	if err != nil {
 		fmt.Printf("Failed To Server: %v\n\n", err)
 		return
 	}
-	
+
 }
