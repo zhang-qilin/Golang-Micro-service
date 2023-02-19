d7652c5526362e7966686fda6bfb850e1f445627
张齐林 <3323816129@qq.com>
2023-02-19 14:35:30 +0800

    创建grpc服务端实例

---
+++ gRPC1/ms-proto/grpc_service.go
@@ -0,0 +1,35 @@
+/*
+* @Time ： 2023-02-19 13:47
+* @Auth ： 张齐林
+* @File ：grpc_service.go
+* @IDE ：GoLand
+ */
+package main
+
+import (
+	"fmt"
+	"log"
+	"ms-proto/service"
+	"net"
+	
+	"google.golang.org/grpc"
+)
+
+func main() {
+	rpcServer := grpc.NewServer()
+	
+	
+	service.RegisterProdServiceServer(rpcServer,service.ProductService)
+	
+	listener, err := net.Listen("tcp",":8002")
+	if err != nil {
+		log.Fatal("启动监听出错",err)
+		return
+	}
+	err = rpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("启动服务出错",err)
+		return
+	}
+	fmt.Println("启动RPC成功...")
+}
