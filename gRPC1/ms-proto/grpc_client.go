2d273b254aad4d68f1c2a1d1ab28068aba349ff1
张齐林 <3323816129@qq.com>
2023-02-19 14:35:15 +0800

    创建grpc客户端实例

---
+++ gRPC1/ms-proto/grpc_client.go
@@ -0,0 +1,38 @@
+/*
+* @Time ： 2023-02-19 14:18
+* @Auth ： 张齐林
+* @File ：grpc_client.go
+* @IDE ：GoLand
+ */
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"ms-proto/service"
+	
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// 建立连接，端口是服务端开放的端口(8002)
+	conn, err := grpc.Dial(":8002",grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("客户端连接不到服务端,",err)
+		return
+	}
+	// 退出时关闭连接
+	defer conn.Close()
+	// 调用Product.pd.go中的NewProdServiceClient方法
+	prodClient := service.NewProdServiceClient(conn)
+	request := &service.ProductRequest{ProdId: 123456}
+	// 直接调用本地方法一样调用GerProductStock方法
+	stockResponse, err := prodClient.GetProductStock(context.Background(),request)
+	if err != nil {
+		log.Fatal("查询库存失败")
+		return
+	}
+	fmt.Println("查询成功,",stockResponse.ProdStock)
+}
